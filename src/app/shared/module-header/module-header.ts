8ecd6848632aab7807be61c9222fefc8b4554063
Jose C Lopez Briseño <joslos_carlos@hotmail.com>
2025-10-30 14:37:03 -0700

    Header y parte del modulo

--- src/app/shared/module-header/module-header.ts
+++ src/app/shared/module-header/module-header.ts
@@ -1,12 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-module-header',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './module-header.html',
   styleUrl: './module-header.scss',
 })
 export class ModuleHeader {
+  @Input() title: string = '';
+  @Input() icon: string = 'dashboard';
+  @Input() showDownload: boolean = false;
+  @Input() showUploadXml: boolean = false;
+  @Input() extraButtons: { icon: string; label: string; action: string }[] = [];
+
+  @Output() download = new EventEmitter<void>();
+  @Output() uploadXml = new EventEmitter<void>();
+  @Output() customAction = new EventEmitter<string>();
+
+  onDescargar() {
+    console.log('Descargando...');
+  }
+
+  onCargarXml() {
+    console.log('Subiendo XML...');
+  }
+
+  onHeaderAction(action: string) {
+    console.log('Acción:', action);
+  }
 
 }
