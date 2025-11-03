87353a1af9801357ba1bba2b02590dda8765e03b
Jose C Lopez Briseño <joslos_carlos@hotmail.com>
2025-11-03 16:58:05 -0700

    Modal

--- src/app/pages/expenses/expense-modal/expense-modal.ts
+++ src/app/pages/expenses/expense-modal/expense-modal.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ModuleHeader, ModuleHeaderConfig } from '../../../shared/module-header/module-header';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 const HEADER_CONFIG: ModuleHeaderConfig = {
   modal: true
@@ -9,13 +13,22 @@ const HEADER_CONFIG: ModuleHeaderConfig = {
 
 @Component({
   selector: 'app-expense-modal',
-  imports: [CommonModule, ModuleHeader],
+  imports: [CommonModule, ModuleHeader, MatIconModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './expense-modal.html',
   styleUrl: './expense-modal.scss',
 })
 export class ExpenseModal implements AfterViewInit {
   private readonly data = inject(MAT_DIALOG_DATA);
   private readonly dialogRef = inject(MatDialogRef<ExpenseModal>);
+  private readonly fb = inject(FormBuilder)
+
+  form: FormGroup = this.fb.group({
+    concept: ['', Validators.required],
+    date: ['', Validators.required],
+    amount: ['', [Validators.required, Validators.min(0.01)]],
+    supplier_id: [''],
+    project_id: [''],
+  })
 
   readonly headerConfig = HEADER_CONFIG;
 
@@ -23,16 +36,17 @@ export class ExpenseModal implements AfterViewInit {
     console.log('Data', this.data);
   }
 
-  onHeaderAction(action: string) {
-    switch (action) {
-      case 'close':
-        this.cerrarModal();
-        break;
+  save() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
     }
+
+    const payload = this.form.getRawValue();
+    console.log('Payload listo para enviar a backend:', payload);
   }
 
-
-  cerrarModal() {
+  closeModal() {
     this.dialogRef.close();
   }
 }
