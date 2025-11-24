d9b2c78dd78149feb3c189dc6f58730fc1c348a4
Jose C Lopez Briseño <joslos_carlos@hotmail.com>
2025-11-23 19:42:23 -0700

    modales y tabla diseño para confirm

---
+++ src/app/pages/clients/components/client-modal/client-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-modal',
+  imports: [],
+  templateUrl: './client-modal.html',
+  styleUrl: './client-modal.scss',
+})
+export class ClientModal {
+
+}
---
+++ src/app/pages/responsible/components/responsible-modal/responsible-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsible-modal',
+  imports: [],
+  templateUrl: './responsible-modal.html',
+  styleUrl: './responsible-modal.scss',
+})
+export class ResponsibleModal {
+
+}
