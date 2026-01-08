1a2209f26071cc833b9d5d18d22fe1b25758289e
Jose C Lopez Briseño <joslos_carlos@hotmail.com>
2026-01-08 14:57:45 -0700

    avance

--- src/app/pages/expenses/components/expense-form/expense-form.ts
+++ src/app/pages/expenses/components/expense-form/expense-form.ts
@@ -271,14 +271,17 @@ export class ExpenseForm implements OnInit {
 
     const payload = this.buildPayloadFromForm();
 
-    this.expenseService.update(this.expenseId, payload).subscribe({
-      next: (response) => {
-        if (response.success) {
-          this.router.navigateByUrl('/gastos');
-        }
-      },
-      error: (err) => console.error('Error al actualizar gasto:', err),
-    });
+    console.log('VERIFICAR OBNNEJTOS ANTES DE GUARDAR ', payload);
+    
+
+    // this.expenseService.update(this.expenseId, payload).subscribe({
+    //   next: (response) => {
+    //     if (response.success) {
+    //       this.router.navigateByUrl('/gastos');
+    //     }
+    //   },
+    //   error: (err) => console.error('Error al actualizar gasto:', err),
+    // });
   }
 
   // ==========================
