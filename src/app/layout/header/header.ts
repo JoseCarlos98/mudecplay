967ef143c038d201c9d6e9a940c3eed93696c12d
Jose C Lopez Briseño <joslos_carlos@hotmail.com>
2026-01-11 23:12:34 -0700

    hader nombre

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,16 +1,18 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../auth/services/auth.service';
+import { AuthUser } from '../../auth/interfaces/auth.interface';
 @Component({
   selector: 'app-header',
   imports: [CommonModule, MatIconModule, MatToolbarModule, MatButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header implements OnInit {
+export class Header {
   @Output() toggleSidebar = new EventEmitter<void>();
-
-  ngOnInit(): void {}
+  private auth = inject(AuthService);
+  currentUser: AuthUser | null = this.auth.currentUser();
 }
